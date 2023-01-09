fc92f6ebee5d1e206835f8629391aa4dcb0be823
Katrina Tomaszczyk <ktomaszczyk@hotmail.com>
2023-01-10 08:36:14 +1000

    added controllers/routes folder

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } =  require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
 const app = express()
 
-app.get('/api/goals', (req, res) =>  {
-    res.json({ message: 'Get goals' })
-})
+// To get the body data of the message we need middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
 
